0117fe7768896bcbe502652b05b7f2ebb375da6c
Ngo Xuan Hieu <92346485+xhiew@users.noreply.github.com>
2022-07-21 15:05:54 +0700

    Initial Commit

---
+++ PassData_Notifications/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PassData_Notifications
+//
+//  Created by ADMIN on 21/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
